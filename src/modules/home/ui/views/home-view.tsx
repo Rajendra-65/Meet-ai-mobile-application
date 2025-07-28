fba14b14c16835872cc256530c405823c5c20d1d
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-28 11:32:48 +0530

    Successfully setup of the trpc in my meet.AI application

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -3,25 +3,20 @@
 import {authClient} from "@/lib/auth-client";
 import {Button} from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-    const {data : session} = authClient.useSession();
-    const router = useRouter()
-
-    if(!session){
-        return (
-            <p> Loading ....</p>
-        )
-    }
+    const trpc = useTRPC()
+    const {data} = useQuery(trpc.hello.queryOptions({
+        text: "Antonio"
+    }))
 
     return (
         <div className = "flex flex-col p-4 gap-y-4">
-            <p>Logged in as {session.user.name}</p>
-            <Button onClick = {()=>authClient.signOut({
-                fetchOptions : {onSuccess : () => router.push('/sign-in')}})}
-            >
-                SignOut
-            </Button>
+            {
+                data?.greeting
+            }
         </div>
     )
 }
