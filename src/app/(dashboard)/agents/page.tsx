a02aeda33b75ed9a995127966d6423fe49561ad6
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-28 15:33:40 +0530

    Added the agents page updated agents view home view and error state of the application and loading state of the application also the procedure

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,33 @@
+import {dehydrate , HydrationBoundary} from "@tanstack/react-query";
+import {getQueryClient , trpc} from "@/trpc/server"
+import { Suspense } from "react";
+import {    
+        AgentsView, 
+        AgentsViewError, 
+        AgentsViewLoading 
+    } from "@/modules/agents/ui/views/agents-view"
+import {ErrorBoundary} from "react-error-boundary";
+
+const Page = async () => {
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+    return(
+        <HydrationBoundary state = {dehydrate(queryClient)}>
+            <Suspense 
+                fallback = {
+                    <AgentsViewLoading/>
+                }
+            >
+                <ErrorBoundary
+                    fallback = {<AgentsViewError/>}
+                >
+                    <AgentsView/>  
+                </ErrorBoundary>
+            </Suspense>
+        </HydrationBoundary>
+    )
+}
+
+export default Page
