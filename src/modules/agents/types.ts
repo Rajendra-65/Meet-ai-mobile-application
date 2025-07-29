b5e449145f214946111a4d13d9a0ee4407b63748
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-29 12:54:12 +0530

    Agents Form Creation also updation and the unauthorized access check

---
+++ src/modules/agents/types.ts
@@ -0,0 +1,9 @@
+import {inferRouterOutputs} from "@trpc/server";
+
+import type {AppRouter} from "@/trpc/routers/_app";
+
+
+// For matching the types returned by the getOne method...
+
+export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
+
