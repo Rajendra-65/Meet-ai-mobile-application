11a0aae7b53b6a24f379fde2fcebbd47ec9f2cef
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-29 17:37:40 +0530

    Pagination of Agents

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -1,9 +1,10 @@
 import { db } from "@/db";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import { and,count,desc,eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import {createTRPCRouter,  protectedProcedure} from "@/trpc/init";
 import { agents } from "@/db/schema";
 import { agentsInsertSchema } from "../schema";
 import { z } from "zod";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 
 export const agentsRouter = createTRPCRouter({
 
@@ -21,16 +22,58 @@ export const agentsRouter = createTRPCRouter({
 
     // Todo : getmany to use protectedProcedure
 
-    getMany : protectedProcedure . query(async () => {
-        const data = await db
+    getMany : protectedProcedure 
+    .input(z.object({
+       page : z.number().default(DEFAULT_PAGE),
+       pageSize : z
+            .number()
+            .min(MIN_PAGE_SIZE)
+            .max(MAX_PAGE_SIZE)
+            .default(DEFAULT_PAGE_SIZE),
+        search : z.string().nullish() 
+     })
+    )
+    .query(async({ctx,input})=>{
+        const {search, page, pageSize} = input;
+        const data = await db.select({
+            meetingCount : sql<number>`5`,
+            ...getTableColumns(agents)
+        })
+        .from(agents)
+        .where(
+            and(
+                eq(agents.userId , ctx.auth.user.id),
+                search ? ilike(agents.name, `%${input.search}%`) : undefined
+            )
+        )
+        .orderBy(
+            desc(agents.createdAt) , desc(agents.id)
+        )
+        .limit(pageSize)
+        .offset((page-1) * pageSize)
+
+        const [totalCount] = await db
             .select({
-                meetingCount : sql<number>`5`,
-                ...getTableColumns(agents),
+                count : count()
             })
             .from(agents)
+            .where(
+                and(
+                    eq(agents.userId, ctx.auth.user.id),
+                    search ? ilike(agents.name, `%${search}%`) : undefined,
+                )
+            );
 
-        return data;
-    }),
+        const totalPages = Math.ceil(totalCount.count/pageSize);
+
+
+        return {
+            items : data,
+            total : totalCount.count,
+            totalPages,
+        };
+    })
+    ,
 
     create : protectedProcedure
         .input(agentsInsertSchema)
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -13,18 +13,18 @@ import { EmptyState } from "@/components/empty-state";
 
 export const  AgentsView = () => {
     const trpc = useTRPC();
-    const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+    const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions({}));
 
     return(
         <div
             className = "flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4"
         >
             <DataTable
-                data = {data}
+                data = {data.items}
                 columns = {columns}
             />
             {
-                data.length === 0 && (
+                data.items.length === 0 && (
                     <EmptyState
                         title = "Create your first Agent"
                         description = "Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
