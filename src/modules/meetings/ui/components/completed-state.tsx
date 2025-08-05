d18034a63e478828c47f8364d28971762c958c70
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-08-05 13:06:04 +0530

    Modified layout and the completed state for the completed meetings

--- src/modules/meetings/ui/components/completed-state.tsx
+++ src/modules/meetings/ui/components/completed-state.tsx
@@ -1,182 +1,164 @@
 import Markdown from "react-markdown";
-import Link from "next/link"
-import {ScrollArea, ScrollBar} from "@/components/ui/scroll-area";
-import {Tabs , TabsContent, TabsList , TabsTrigger} from "@/components/ui/tabs";
+import Link from "next/link";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { format } from "date-fns";
 
-import { MeetingGetOne } from "../../types"
+import { MeetingGetOne } from "../../types";
 import { BookOpenTextIcon, ClockFadingIcon, FileVideoIcon } from "lucide-react";
 import { GeneratedAvatar } from "@/components/generated-avatar";
-
 import { Badge } from "@/components/ui/badge";
 
 import {
-    SparklesIcon,
-    FileTextIcon,
-    BookOpenTexticon,
-    FileVideoicon,
-    ClockFadingicon
+  SparklesIcon,
+  FileTextIcon,
 } from "lucide-react";
+
 import { formatDuration } from "@/lib/utils";
-// import { format } from "path";
 
 interface Props {
-    data : MeetingGetOne;
+  data: MeetingGetOne;
 }
 
-export const CompletedState = ( {data} : Props) => {
-    return(
-        <div className = "flex flex-col gayp-y-4">
-            <Tabs defaultValue = "summary">
-                <div className = "bg-white rounded-lg border px-3">
-                    <ScrollArea>
-                        <TabsList className = "p-0 bg-background justify-start rounded-none h-13">
-                            <TabsTrigger 
-                                value = "summary"
-                                className = "text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
-                            >
-                                <BookOpenTextIcon/>
-                                Summary
-                            </TabsTrigger>
-                            <TabsTrigger 
-                                value = "transcript"
-                                className = "text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
-                            >
-                                <FileTextIcon/>
-                                Transcript
-                            </TabsTrigger>
-                            <TabsTrigger 
-                                value = "recording"
-                                className = "text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
-                            >
-                                <FileVideoIcon/>
-                                Recording
-                            </TabsTrigger>
-                            <TabsTrigger 
-                                value = "chat"
-                                className = "text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
-                            >
-                                <SparklesIcon/>
-                                Ask AI
-                            </TabsTrigger>
-                        </TabsList>
-                    </ScrollArea>
-                </div>
-                <TabsContent value = "recording">
-                    <div className = "bg-white rounded-lg border px-4 py-5">
-                        <video
-                            src = {data.recordingUrl!}
-                            className = "w-full rounded-lg"
-                            controls
-                        />
-                    </div>
-                </TabsContent>
-                <TabsContent value = "summary">
-                    <div className = "bg-white rounded-lg border">
-                        <div className = "px-4 py-5 gap-y-5 flex flex-col col-span-5">
-                            <h2 className = "text-2xl font-medium capitalize">{data.name}</h2>
-                            <div className = "flex gap-x-2 items-center">
-                                <Link
-                                    href = {`/agents/${data.agent.id}`}
-                                    className = "flex items-center gap-x-2 underline underline-offset-4 capitalize"
-                                >
-                                    <GeneratedAvatar
-                                        variant = "botttsNeutral"
-                                        seed = {data.agent.name}
-                                        className = "size-5"
-                                    />
-                                    {data.agent.name}
-                                </Link>
-                                <p>{data.startedAt ? format(data.startedAt,"PPP") : ""}</p>
-                            </div>
-                            <div className = "flex gap-x-2 items-center">
-                                <SparklesIcon className = "size-4">
-                                    <p>General Summary</p>
-                                </SparklesIcon>
-                            </div>
-                            <Badge
-                                variant = "outline"
-                                className = "flex items-center gap-x-2 [&>svg]:size-4"
-                            >
-                                <ClockFadingIcon
-                                    className = "text-blue-700"
-                                />
-                                {
-                                    data.duration ? formatDuration(data.duration) : "No Duration"
-                                }
-                                <div>
-                                    <Markdown
-                                        components = {{
-                                            h1 : (props) => (
-                                                <h1 
-                                                    className = "text-2xl font-medium mb-6"
-                                                    {...props}
-                                                />
-                                            ),
-                                            h2 : (props) => (
-                                                <h1 
-                                                    className = "text-xl font-medium mb-6"
-                                                    {...props}
-                                                />
-                                            ),
-                                            h3 : (props) => (
-                                                <h1 
-                                                    className = "text-lg font-medium mb-6"
-                                                    {...props}
-                                                />
-                                            ),
-                                            h4 : (props) => (
-                                                <h1 
-                                                    className = "text-base font-medium mb-6"
-                                                    {...props}
-                                                />
-                                            ),
-                                            p : (props) => (
-                                                <p className = "mb-6 leading-relaxed" {...props}/>
-                                            ),
-                                            ul : (props) =>(
-                                                <ul className = "list-disc list-inside mb-6" {...props}/>
-                                            ),
-                                            ol : (props) => (
-                                                <ol
-                                                    className = "list-decimal list-inside mb-6"
-                                                    {...props}
-                                                />
-                                            ),
-                                            li : (props) => (
-                                                <li 
-                                                    className = "mb-1"
-                                                    {...props}
-                                                />
-                                            ),
-                                            strong : (props) => (
-                                                <strong 
-                                                    className = "font-semibold"
-                                                    {...props}
-                                                />
-                                            ),
-                                            code : (props) => (
-                                                <code 
-                                                    className = "bg-gray-100 px-1 py-0.5 rounded"
-                                                    {...props}
-                                                />
-                                            ),
-                                            blockquote : (props) => (
-                                                <blockquote 
-                                                    className = "border-l-4 pl-4 italic my-4"
-                                                    {...props}
-                                                />
-                                            )
-                                        }}
-                                    >
-                                        {data.summary}
-                                    </Markdown>
-                                </div>
-                            </Badge>
-                        </div>
-                    </div>
-                </TabsContent>
-            </Tabs>
+export const CompletedState = ({ data }: Props) => {
+  return (
+    <div className="flex flex-col gap-y-4 max-w-screen px-2 sm:px-4 lg:px-6 w-full">
+      <Tabs defaultValue="summary">
+        <div className="bg-white rounded-lg border px-3">
+          <ScrollArea>
+            <TabsList className="p-0 bg-background justify-start rounded-none h-13 overflow-x-auto flex-nowrap sm:flex-wrap">
+              <TabsTrigger
+                value="summary"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <BookOpenTextIcon />
+                Summary
+              </TabsTrigger>
+              <TabsTrigger
+                value="transcript"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <FileTextIcon />
+                Transcript
+              </TabsTrigger>
+              <TabsTrigger
+                value="recording"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <FileVideoIcon />
+                Recording
+              </TabsTrigger>
+              <TabsTrigger
+                value="chat"
+                className="text-muted-foreground rounded-none bg-background data-[state=active]:shadow-none border-b-2 border-transparent data-[state=active]:border-b-primary data-[state=active]:text-accent-foreground h-full hover:text-accent-foreground"
+              >
+                <SparklesIcon />
+                Ask AI
+              </TabsTrigger>
+            </TabsList>
+          </ScrollArea>
         </div>
-    )
-}
+
+        <TabsContent value="recording">
+          <div className="bg-white rounded-lg border px-4 py-5">
+            <video
+              src={data.recordingUrl!}
+              className="w-full rounded-lg max-h-[50vh] sm:max-h-[60vh] object-contain"
+              controls
+            />
+          </div>
+        </TabsContent>
+
+        <TabsContent value="summary">
+          <div className="bg-white rounded-lg border w-auto">
+            <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+              <h2 className="text-lg sm:text-xl md:text-2xl font-medium capitalize flex flex-wrap break-words">
+                {data.name}
+              </h2>
+
+              <div className="flex gap-x-2 items-center flex-wrap text-sm sm:text-base">
+                <Link
+                  href={`/agents/${data.agent.id}`}
+                  className="flex items-center gap-x-2 underline underline-offset-4 capitalize"
+                >
+                  <GeneratedAvatar
+                    variant="botttsNeutral"
+                    seed={data.agent.name}
+                    className="size-5"
+                  />
+                  {data.agent.name}
+                </Link>
+                <p>{data.startedAt ? format(new Date(data.startedAt), "PPP") : ""}</p>
+              </div>
+
+              <div className="flex gap-x-2 items-center">
+                <SparklesIcon className="size-4" />
+                <p>General Summary</p>
+              </div>
+
+              <Badge
+                variant="outline"
+                className="flex items-center gap-x-2 [&>svg]:size-4 w-auto"
+              >
+                <ClockFadingIcon className="text-blue-700" />
+                {data.duration ? formatDuration(data.duration) : "No Duration"}
+              </Badge>
+
+              <div className="overflow-x-auto w-full">
+                <Markdown
+                  components={{
+                    h1: (props) => (
+                      <h1 className="text-2xl font-medium mb-6" {...props} />
+                    ),
+                    h2: (props) => (
+                      <h2 className="text-xl font-medium mb-6" {...props} />
+                    ),
+                    h3: (props) => (
+                      <h3 className="text-lg font-medium mb-6" {...props} />
+                    ),
+                    h4: (props) => (
+                      <h4 className="text-base font-medium mb-6" {...props} />
+                    ),
+                    p: (props) => (
+                      <p
+                        className="mb-6 leading-relaxed break-words text-pretty max-w-full"
+                        {...props}
+                      />
+                    ),
+                    ul: (props) => (
+                      <ul className="list-disc list-inside mb-6" {...props} />
+                    ),
+                    ol: (props) => (
+                      <ol
+                        className="list-decimal list-inside mb-6"
+                        {...props}
+                      />
+                    ),
+                    li: (props) => <li className="mb-1" {...props} />,
+                    strong: (props) => (
+                      <strong className="font-semibold" {...props} />
+                    ),
+                    code: (props) => (
+                      <code
+                        className="bg-gray-100 px-1 py-0.5 rounded"
+                        {...props}
+                      />
+                    ),
+                    blockquote: (props) => (
+                      <blockquote
+                        className="border-l-4 pl-4 italic my-4"
+                        {...props}
+                      />
+                    ),
+                  }}
+                >
+                  {data.summary}
+                </Markdown>
+              </div>
+            </div>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
