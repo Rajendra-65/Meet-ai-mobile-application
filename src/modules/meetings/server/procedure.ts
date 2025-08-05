b4990d5fb496316c8026096d4af61c70841723ea
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-08-05 15:57:00 +0530

    Chat UI integration and modification for chat ui

--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -10,8 +10,16 @@ import { MeetingStatus, StreamTranscriptItem } from "../types";
 import { streamVideo } from "@/lib/stream-video";
 import { generatedAvatarUri } from "@/lib/avatar";
 import JSONL from "jsonl-parse-stringify";
+import { streamChat } from "@/lib/stream-chat";
 
 export const meetingsRouter = createTRPCRouter({
+    generateChatToken : protectedProcedure.mutation(async({ctx}) => {
+        const token = streamChat.createToken(ctx.auth.user.id);
+        await streamChat.upsertUser({
+            id : ctx.auth.user.id,
+            role : "admin"
+        })
+    }),
     getTranscript: protectedProcedure
         .input(z.object({
             id: z.string()
